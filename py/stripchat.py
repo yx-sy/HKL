44a4e1c185fe02ca16519a3ac18b30402cde7538
FGBLH <pengli08211@gmail.com>
2026-07-01 22:31:43 +0800

    Update stripchat.py

--- py/stripchat.py
+++ py/stripchat.py
@@ -24,7 +24,7 @@ class Spider(Spider):
         self.headers = {
             'Origin': origin,
             'Referer': f"{origin}/",
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+            'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
         }
         self.stripchat_key = self.decode_key_compact()
         # 缓存字典
